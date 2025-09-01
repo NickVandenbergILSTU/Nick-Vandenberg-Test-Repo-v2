781cea29258e969338ea488e5f3831863ce41791
Nick Vandenberg <nvandenberg2021@gmail.com>
2025-08-31 21:33:52 -0500

    added html opening script

--- test.sh
+++ test.sh
@@ -2,4 +2,11 @@
 
 echo "Opening HTML file"
 
-xdg-open index.html
+# This may be the command for opening html in the pipeline, not tested yet
+# xdg-open index.html
+
+# Use locally in VSCode
+start index.html
+
+# IDK what this is
+# explorer.exe test.html
